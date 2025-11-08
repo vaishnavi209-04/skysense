7456abbe5a11666586fe7b541d795918b0177774
Vaishnavi Mishra <vaishnavimishra986@gmail.com>
2025-11-08 21:10:45 +0530

    feat:ready for deplyment

--- server/index.js
+++ server/index.js
@@ -79,6 +79,8 @@ app.get("/api/background/:query", async (req, res) => {
 
 
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`✅ Server running on port ${PORT}`)
+);
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
